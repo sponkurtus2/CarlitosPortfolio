fb087b30412b9bf4a22d55bc2e215804c9002bc6
sponkurtus2 <sponnkurtus3@gmail.com>
2024-05-04 22:55:22 -0600

    ^^

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config';
+import swup from '@swup/astro';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [swup({
+      theme: 'fade',
+      containers: ['main', 'nav', 'body'],
+      cache: true,
+      preload: false,
+      accessibility: true,
+      morph: false,
+      parallel: false,
+      progress: false,
+      routes: false,
+      smoothScrolling: true,
+      updateBodyClass: false,
+      updateHead: true,
+      reloadScripts: true,
+      debug: false,
+      loadOnIdle: true,
+      globalInstance: false,
+    })]
+});
