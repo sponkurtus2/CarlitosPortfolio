7ea9b7af64a71e61dd64451d59e5cf52c544e71a
sponkurtus2 <sponnkurtus3@gmail.com>
2024-10-06 14:54:44 -0600

    Build working :D

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,35 @@
 import { defineConfig } from 'astro/config';
 import swup from '@swup/astro';
+import node from "@astrojs/node";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [swup({
-      theme: 'fade',
-      containers: ['main', 'nav', 'body'],
-      cache: true,
-      preload: false,
-      accessibility: true,
-      morph: false,
-      parallel: false,
-      progress: false,
-      routes: false,
-      smoothScrolling: true,
-      updateBodyClass: false,
-      updateHead: true,
-      reloadScripts: true,
-      debug: false,
-      loadOnIdle: true,
-      globalInstance: false,
-      Content: true,
-    })]
+  server: {
+    port: parseInt(process.env.PORT) || 4321,
+    host: true
+  },
+  integrations: [swup({
+    theme: 'fade',
+    containers: ['main', 'nav', 'body'],
+    cache: true,
+    preload: false,
+    accessibility: true,
+    morph: false,
+    parallel: false,
+    progress: false,
+    routes: false,
+    smoothScrolling: true,
+    updateBodyClass: false,
+    updateHead: true,
+    reloadScripts: true,
+    debug: false,
+    loadOnIdle: true,
+    globalInstance: false
+  }), mdx()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
