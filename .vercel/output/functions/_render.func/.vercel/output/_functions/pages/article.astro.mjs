4f4c9725e35ed9c5caa481a9367d1cfc5842428d
sponkurtus2 <sponnkurtus3@gmail.com>
2024-10-14 14:26:02 -0600

    Removed vercel folder

--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/article.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../chunks/article_kLZ8rzB7.mjs';
-export { renderers } from '../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/articles/lifeisgood.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../chunks/LifeIsGood_a5OKH6q6.mjs';
-export { renderers } from '../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
